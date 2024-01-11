a3c33e861fbba3cf94a6ac534642a3aea07a5970
Cong Kha <hoangcongkha96@gmail.com>
2024-01-11 20:07:54 +0700

    add search friend

--- src/Components/menuMain/Post.tsx
+++ src/Components/menuMain/Post.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import "./Post.css";
+
 export default function mainContainer() {
   return (
     <main className='Post-container'>
       <div className='srories-post'>
-        <h2> Stories</h2>
-        <div>
-          
-        </div>
+        <h3 className='name-port'>Stories</h3>
+        <div className='video-stores-post'>
 
+        </div>
+        
       </div>
 
     </main>

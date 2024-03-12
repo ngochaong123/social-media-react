5598e007f54f7e2e0e2994aba5a13a2967841809
CongKha <hoangcongkha96@gmail.com>
2024-03-12 21:40:42 +0700

    add frame avatar

--- src/Components/header/Notification.tsx
+++ src/Components/header/Notification.tsx
@@ -14,81 +14,84 @@ import avatar7 from "../../img/sidebarMenu/notification/img7.jpg";
 export default function Notification() {
 
     return (
-        <main className="notification-container"> {/* frames notification  */}
-          <h1 className="title-Notifca"> Thông báo </h1>
+          <main className="notification-Localtion">
+             <div className="notification-container"> {/* frames notification  */}
+                    <h1 className="title-Notifca"> Thông báo </h1>
 
-          {/* Notification group 1 */}
-          <div className="group-notification">
-               <div className="notifica">
-                    <img src={avatar1} className="avatar-notifica"></img>
-                    <p> Game ROBLOX đã mời 2 người mới vào nhóm </p>
-               </div>
-               <div className="group-time-notification"> 1 tuần trước </div>
-          </div>
-          
-          {/* Notification group 2 */}
-          <div className="group-notification">
-               <div className="notifica">
-                    <img src={avatar2} className="avatar-notifica"></img>
-                    <p> Liên quân mobile thắng bại tại kỹ năng ra mắt vị tướng mới </p>
-               </div>
-               <div className="group-time-notification"> 2 tuần trước </div>
-          </div>
-          
-          {/* Notification group 3 */}
-          <div className="group-notification">
-               <div className="notifica">
-                    <img src={avatar3} className="avatar-notifica"></img>
-                    <p> Liên minh huyện thoại ra mắt vị tướng mới </p>
-               </div>
-               <div className="group-time-notification"> 5 giờ trước </div>
-          </div>
-          
-          {/* Notification group 4 */}
-          <div className="group-notification">
-               <div className="notifica">
-                    <img src={avatar4} className="avatar-notifica"></img>
-                    <p> Khám phá thiên nhiên Bạn Nam đã đăng 6 tấm ảnh mới về nước Ý </p>
-               </div>
-               <div className="group-time-notification"> 2 phút trước </div>
-          </div>
+                    {/* Notification group 1 */}
+                    <div className="group-notification">
+                         <div className="notifica">
+                              <img src={avatar1} className="avatar-notifica"></img>
+                              <p> Game ROBLOX đã mời 2 người mới vào nhóm </p>
+                         </div>
+                         <div className="group-time-notification"> 1 tuần trước </div>
+                    </div>
+                    
+                    {/* Notification group 2 */}
+                    <div className="group-notification">
+                         <div className="notifica">
+                              <img src={avatar2} className="avatar-notifica"></img>
+                              <p> Liên quân mobile thắng bại tại kỹ năng ra mắt vị tướng mới </p>
+                         </div>
+                         <div className="group-time-notification"> 2 tuần trước </div>
+                    </div>
+                    
+                    {/* Notification group 3 */}
+                    <div className="group-notification">
+                         <div className="notifica">
+                              <img src={avatar3} className="avatar-notifica"></img>
+                              <p> Liên minh huyện thoại ra mắt vị tướng mới </p>
+                         </div>
+                         <div className="group-time-notification"> 5 giờ trước </div>
+                    </div>
+                    
+                    {/* Notification group 4 */}
+                    <div className="group-notification">
+                         <div className="notifica">
+                              <img src={avatar4} className="avatar-notifica"></img>
+                              <p> Khám phá thiên nhiên Bạn Nam đã đăng 6 tấm ảnh mới về nước Ý </p>
+                         </div>
+                         <div className="group-time-notification"> 2 phút trước </div>
+                    </div>
 
-          {/* Notification group 5 */}
-          <div className="group-notification">
-               <div className="notifica">
-                    <img src={avatar5} className="avatar-notifica"></img>
-                    <p> CLB Vật lý và những ứng dụng thực tế  </p>
-               </div>
-               <div className="group-time-notification"> 1 tuần trước </div>
-          </div>
-          
-          {/* Notification group 6 */}
-          <div className="group-notification" style={{marginBottom:"20px"}}>
-               <div className="notifica">
-                    <img src={avatar6} className="avatar-notifica"></img>
-                    <p>Tin giải trí trong cuộc sống Thông báo truyền hình sẽ phát trực tiếp vào 8 giờ tối hôm nay </p>
-               </div>
-               <div className="group-time-notification"> 6 tiếng trước </div>
-          </div>
+                    {/* Notification group 5 */}
+                    <div className="group-notification">
+                         <div className="notifica">
+                              <img src={avatar5} className="avatar-notifica"></img>
+                              <p> CLB Vật lý và những ứng dụng thực tế  </p>
+                         </div>
+                         <div className="group-time-notification"> 1 tuần trước </div>
+                    </div>
+                    
+                    {/* Notification group 6 */}
+                    <div className="group-notification" style={{marginBottom:"20px"}}>
+                         <div className="notifica">
+                              <img src={avatar6} className="avatar-notifica"></img>
+                              <p>Tin giải trí trong cuộc sống Thông báo truyền hình sẽ phát trực tiếp vào 8 giờ tối hôm nay </p>
+                         </div>
+                         <div className="group-time-notification"> 6 tiếng trước </div>
+                    </div>
 
-          {/* Notification group 7 */}
-          <div className="group-notification" style={{marginBottom:"20px"}}>
-               <div className="notifica">
-                    <img src={avatar7} className="avatar-notifica"></img>
-                    <p>Âm nhạc reimix đăng bài hát mới Nothing can't stop me </p>
-               </div>
-               <div className="group-time-notification"> 6 tiếng trước </div>
-          </div>
+                    {/* Notification group 7 */}
+                    <div className="group-notification" style={{marginBottom:"20px"}}>
+                         <div className="notifica">
+                              <img src={avatar7} className="avatar-notifica"></img>
+                              <p>Âm nhạc reimix đăng bài hát mới Nothing can't stop me </p>
+                         </div>
+                         <div className="group-time-notification"> 6 tiếng trước </div>
+                    </div>
 
-          {/* Notification group 8 */}
-          <div className="group-notification" style={{marginBottom:"20px"}}>
-               <div className="notifica">
-                    <img src={avatar6} className="avatar-notifica"></img>
-                    <p>Tin giải trí trong cuộc sống Thông báo truyền hình sẽ phát trực tiếp vào 8 giờ tối hôm nay </p>
-               </div>
-               <div className="group-time-notification"> 5 ngày trước </div>
-          </div>
+                    {/* Notification group 8 */}
+                    <div className="group-notification" style={{marginBottom:"20px"}}>
+                         <div className="notifica">
+                              <img src={avatar6} className="avatar-notifica"></img>
+                              <p>Tin giải trí trong cuộc sống Thông báo truyền hình sẽ phát trực tiếp vào 8 giờ tối hôm nay </p>
+                         </div>
+                         <div className="group-time-notification"> 5 ngày trước </div>
+                    </div>
 
-        </main>
+               </div>  
+          </main>
+        
     )
 }
--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -1,11 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Outlet, Link} from "react-router-dom";
+import React, { useState, useRef } from 'react';
+
+import { Outlet, Link } from "react-router-dom";
 import Logo from "../../img/sidebarMenu/Logo.png";
 import Magnifier from "../../img/sidebarMenu/magnifier.png";
 import mes from "../../img/sidebarMenu/messenger.png";
 import Bell from "../../img/sidebarMenu/bell.png";
 import User from "../../img/sidebarMenu/user1.png";
 import Notification from "./Notification";
+import Avatar from "../../img/Chat/avatar-user.png";
+import Exit from "../../img/sidebarMenu/exit.png";
+import Sun from "../../img/sidebarMenu/sun.png";
+import Setting from "../../img/sidebarMenu/settings.png";
+
+import { useNavigate } from 'react-router-dom';
+
 import "./header.css";
 
 const Header = () => {
@@ -13,57 +21,120 @@ const Header = () => {
     const notificationRef = useRef(null);
 
     const handleNotificationClick = () => {
-        // Toggle the showNotification state when clicking the bell button
         setShowNotification(!showNotification);
+        setShowTabUser(false);
     };
 
-  return (
-    <div>
-        <div className='head-log'>
-            {/* return to menu */}
-            <Link to="/Menu">
-                <img src={Logo} alt="Logo" className="Logo-Log" />
-            </Link>
-            
-            {/* search friend, group */}
-            <div className="input-log-container">
-                {/* icon search */}
-                <div className="search-icon">
-                    <img src={Magnifier}/>
-                </div>
-                <input className="input-log" type="text" placeholder="Tìm kiếm" />
-            </div>
+    const [showTabUser, setShowTabUser] = useState(false);
+    const noTabUser = useRef(null);
 
-            {/* chat to friend */}
-            <div className='enter-log-container'>
-            <Link to="/Menu/Chat">
-                <button className='enter-log'><img className='icon-log' src={mes} alt="Messenger Icon" /></button>
-            </Link>
-            
-            {/* Notification code in header */}
-            <div>
-                {/* Nút Bell */}
-                <button onClick={handleNotificationClick} className='enter-log'>
-                    <img className='icon-log' src={Bell} alt="Bell Icon" />
-                </button>
+    const handleTabClick = () => {
+        setShowTabUser(!showTabUser);
+        setShowNotification(false); 
+    };
 
-                {/* Component Notification, được điều chỉnh bởi showNotification */}
-                {showNotification && (
-                    <div ref={notificationRef}>
-                        <Notification />
+    // call link
+    const navigate = useNavigate();
+
+    // link to personalPage
+    const personalPage = () => {
+        navigate('/Menu/User');
+    };
+
+    // login out
+    function LoginOut (){
+        navigate("/Login");
+    }
+
+    return (
+        <div>
+            <div className='head-log'>
+                <Link to="/Menu">
+                    <img src={Logo} alt="Logo" className="Logo-Log" />
+                </Link>
+
+                <div className="input-log-container">
+                    <div className="search-icon">
+                        <img src={Magnifier} alt="Search Icon" />
                     </div>
-                )}
-            </div>
+                    <input className="input-log" type="text" placeholder="Tìm kiếm" />
+                </div>
 
-            {/* user infomation */}
-            <Link to="/Menu/User">
-                <button className='enter-log'><img className='icon-log' src={User} alt="User Icon" /></button>
-            </Link>
+                <div className='enter-log-container'>
+                    <Link to="/Menu/Chat">
+                        <button className='enter-log'><img className='icon-log' src={mes} alt="Messenger Icon" /></button>
+                    </Link>
+
+                    <div>
+                        <button onClick={handleNotificationClick} className='enter-log'>
+                            <img className='icon-log' src={Bell} alt="Bell Icon" />
+                        </button>
+
+                        {showNotification && (
+                            <div ref={notificationRef}>
+                                <Notification />
+                            </div>
+                        )}
+                    </div>
+
+                    <div>
+                        <button onClick={handleTabClick} className='enter-log'><img className='icon-log' src={User} alt="User Icon" /></button>
+
+                        {showTabUser && (
+                            <div ref={noTabUser} className='tabUserLocal'>
+                                <div className='tabUser'>
+                                    {/* personal page */}
+                                    <button className='ClicktabUser' onClick={personalPage}>
+                                        <img src={Avatar} className='personalPage' 
+                                            style={{
+                                                width:"36px",
+                                                height:"36px",
+                                                marginLeft:"2px"
+                                            }}
+                                        ></img>
+                                        <div className='NametabUser' style={{marginTop:"-4px"}}> Trang cá nhân </div>
+                                    </button>
+                                    
+                                    {/* mode light or night */}
+                                    <button className='ClicktabUser' >
+                                        <div className='iconTab'>
+                                            <img src={Sun} className='personalPage'></img>
+                                        </div>    
+                                        <div className='NametabUser'> Chế độ sáng tối  </div>
+                                    </button>
+                                    
+                                    {/* setting */}
+                                    <button className='ClicktabUser'>
+                                        <div className='iconTab'>
+                                             <img src={Setting} className='personalPage'></img>
+                                        </div>
+                                        <div className='NametabUser'> Cài đặt  </div>
+                                    </button>
+
+                                    {/* login out account */}
+                                    <button className='ClicktabUser' onClick={LoginOut}>
+                                        <div className='iconTab'>
+                                            <img src={Exit} className='personalPage' style={{
+                                                width:"23px",
+                                                height:"23px",
+                                                marginRight:"-1px",
+                                                marginTop:"2px"
+                                            }}
+                                            ></img>
+                                        </div>
+                                        <div className='NametabUser'> Thoát tài khoản </div>
+                                    </button>
+
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+                </div>
             </div>
+            <Outlet />
         </div>
-        <Outlet />
-    </div>
-  )
+    )
 }
 
 export default Header;
